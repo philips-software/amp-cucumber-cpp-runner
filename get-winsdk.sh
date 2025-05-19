739cc17efb796fde4542283016015e4b200b8101
Timmer, Daan <daan.timmer@philips.com>
2025-05-19 13:15:36 +0000

    fix: update ccache key and add crt-version to get-winsdk.sh for consistency

--- get-winsdk.sh
+++ get-winsdk.sh
@@ -1,1 +1,1 @@
-xwin --accept-license --temp --sdk-version "10.0.26100" splat --preserve-ms-arch-notation --include-debug-libs --output /winsdk
+xwin --accept-license --temp --sdk-version "10.0.26100" --crt-version "14.43.17.13" splat --preserve-ms-arch-notation --include-debug-libs --output /winsdk
