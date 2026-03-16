325fb4ca3f55aff346f976f4b91ea8100ea0fb65
Daan Timmer <8293597+daantimmer@users.noreply.github.com>
2026-03-16 11:18:46 +0100

    chore: update dependencies (#305)

--- cucumber_cpp/library/util/ArgumentGroupToMessageGroup.cpp
+++ cucumber_cpp/library/util/ArgumentGroupToMessageGroup.cpp
@@ -1,7 +1,10 @@
 #include "cucumber_cpp/library/util/ArgumentGroupToMessageGroup.hpp"
 #include "cucumber/messages/group.hpp"
 #include "cucumber_cpp/library/cucumber_expression/Group.hpp"
+#include <optional>
 #include <ranges>
+#include <utility>
+#include <vector>
 
 namespace cucumber_cpp::library::util
 {
@@ -9,7 +12,7 @@ namespace cucumber_cpp::library::util
     {
         auto messageChildren = argumentGroup.children | std::views::transform(ArgumentGroupToMessageGroup);
         return {
-            .children = { messageChildren.begin(), messageChildren.end() },
+            .children = std::optional<std::vector<cucumber::messages::group>>{ std::in_place, messageChildren.begin(), messageChildren.end() },
             .start = argumentGroup.start,
             .value = argumentGroup.value,
         };
