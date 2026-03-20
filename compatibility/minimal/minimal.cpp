dcda94674296087680d5a9a03e169582b84f268f
Timmer, Daan <daan.timmer@philips.com>
2026-03-20 16:32:26 +0000

    refactor: mark parameters as maybe unused in step definitions

--- compatibility/minimal/minimal.cpp
+++ compatibility/minimal/minimal.cpp
@@ -1,7 +1,7 @@
 #include "cucumber_cpp/Steps.hpp"
 #include <cstdint>
 
-STEP(R"(I have {int} cukes in my belly)", (std::int32_t number))
+STEP(R"(I have {int} cukes in my belly)", ([[maybe_unused]] std::int32_t number))
 {
     // no-op
 }
