b70ac5e98a269ff4b8ba1a8b24db8997304d777e
Timmer, Daan <daan.timmer@philips.com>
2026-07-21 11:30:01 +0000

    refactor: simplify dependency declarations using ccr_dependency macro

--- cmake/dependencies.cpm.cmake
+++ cmake/dependencies.cpm.cmake
@@ -1,37 +1,27 @@
-# renovate: datasource=github-tags packageName=cpm-cmake/CPM.cmake versioning=semver
-set(CPM_DOWNLOAD_TAG_DIGEST     v0.43.1    456cb6754daaa010d57444d0c8ce6d95ecf006ab)
-# renovate: datasource=github-tags packageName=cucumber/gherkin versioning=semver
-set(CUCUMBER_GHERKIN_TAG_DIGEST v42.0.0    f2f7b51ae6a886aefc1f4a2059e4989b83aab8c8)
-# renovate: datasource=github-tags packageName=google/googletest versioning=semver
-set(GOOGLE_TEST_TAG_DIGEST      v1.17.0    52eb8108c5bdec04579160ae17225d66034bd723)
-# renovate: datasource=github-tags packageName=CLIUtils/CLI11 versioning=semver
-set(CLI11_TAG_DIGEST            v2.6.2     37bb6edc5317e99af72ef48405e65d9ca5218861)
-# renovate: datasource=github-tags packageName=fmtlib/fmt versioning=semver
-set(LIBFMT_TAG_DIGEST           v12.1.0    407c905e45ad75fc29bf0f9bb7c5c2fd3475976f)
-# renovate: datasource=github-tags packageName=zeux/pugixml versioning=semver
-set(PUGIXML_TAG_DIGEST          v1.15      ee86beb30e4973f5feffe3ce63bfa4fbadf72f38)
-# renovate: datasource=github-tags packageName=abseil/abseil-cpp
-set(ABSEIL_CPP_TAG_DIGEST       20250814.2 0cf0a5c9d12cc3783363ab20f11613e69fd04c9a)
-# renovate: datasource=github-tags packageName=google/re2
-set(RE2_TAG_DIGEST              2025-08-12 0f6c07eae69151e606acb3d9232750c3442dff23)
-# renovate: datasource=github-tags packageName=jbeder/yaml-cpp versioning=semver
-set(YAML_CPP_TAG_DIGEST         v0.9.0     56e3bb550c91fd7005566f19c079cb7a503223cf)
-
-macro(ccr_resolve_tag_digest_version LIST_VAR PREFIX)
-    list(GET ${LIST_VAR} 0 ${PREFIX}_TAG)
-    list(GET ${LIST_VAR} 1 ${PREFIX}_DIGEST)
-    string(REGEX REPLACE "^v" "" ${PREFIX}_VERSION "${${PREFIX}_TAG}")
+macro(ccr_dependency PREFIX TAG DIGEST)
+    set(${PREFIX}_TAG    "${TAG}")
+    set(${PREFIX}_DIGEST "${DIGEST}")
+    string(REGEX REPLACE "^v" "" ${PREFIX}_VERSION "${TAG}")
 endmacro()
 
-ccr_resolve_tag_digest_version(CPM_DOWNLOAD_TAG_DIGEST     CPM_DOWNLOAD)
-ccr_resolve_tag_digest_version(CUCUMBER_GHERKIN_TAG_DIGEST CUCUMBER_GHERKIN)
-ccr_resolve_tag_digest_version(GOOGLE_TEST_TAG_DIGEST      GOOGLE_TEST)
-ccr_resolve_tag_digest_version(CLI11_TAG_DIGEST            CLI11)
-ccr_resolve_tag_digest_version(LIBFMT_TAG_DIGEST           LIBFMT)
-ccr_resolve_tag_digest_version(PUGIXML_TAG_DIGEST          PUGIXML)
-ccr_resolve_tag_digest_version(ABSEIL_CPP_TAG_DIGEST       ABSEIL_CPP)
-ccr_resolve_tag_digest_version(RE2_TAG_DIGEST              RE2)
-ccr_resolve_tag_digest_version(YAML_CPP_TAG_DIGEST         YAML_CPP)
+# renovate: datasource=github-tags packageName=cpm-cmake/CPM.cmake versioning=semver
+ccr_dependency(CPM_DOWNLOAD     v0.43.1    456cb6754daaa010d57444d0c8ce6d95ecf006ab)
+# renovate: datasource=github-tags packageName=cucumber/gherkin versioning=semver
+ccr_dependency(CUCUMBER_GHERKIN v42.0.0    f2f7b51ae6a886aefc1f4a2059e4989b83aab8c8)
+# renovate: datasource=github-tags packageName=google/googletest versioning=semver
+ccr_dependency(GOOGLE_TEST      v1.17.0    52eb8108c5bdec04579160ae17225d66034bd723)
+# renovate: datasource=github-tags packageName=CLIUtils/CLI11 versioning=semver
+ccr_dependency(CLI11            v2.6.2     37bb6edc5317e99af72ef48405e65d9ca5218861)
+# renovate: datasource=github-tags packageName=fmtlib/fmt versioning=semver
+ccr_dependency(LIBFMT           v12.1.0    407c905e45ad75fc29bf0f9bb7c5c2fd3475976f)
+# renovate: datasource=github-tags packageName=zeux/pugixml versioning=semver
+ccr_dependency(PUGIXML          v1.15      ee86beb30e4973f5feffe3ce63bfa4fbadf72f38)
+# renovate: datasource=github-tags packageName=abseil/abseil-cpp
+ccr_dependency(ABSEIL_CPP       20250814.2 0cf0a5c9d12cc3783363ab20f11613e69fd04c9a)
+# renovate: datasource=github-tags packageName=google/re2
+ccr_dependency(RE2              2025-08-12 0f6c07eae69151e606acb3d9232750c3442dff23)
+# renovate: datasource=github-tags packageName=jbeder/yaml-cpp versioning=semver
+ccr_dependency(YAML_CPP         v0.9.0     56e3bb550c91fd7005566f19c079cb7a503223cf)
 
 if(CCR_FETCH_DEPS)
     if(NOT COMMAND CPMAddPackage)
