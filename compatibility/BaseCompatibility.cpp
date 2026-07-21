00ab02e8e250ac44b8a376b5a4e5e688f1a049fe
Timmer, Daan <daan.timmer@philips.com>
2026-07-21 15:09:27 +0000

    refactor: remove compile definitions for source and binary directories in CMakeLists.txt and update BaseCompatibility to use relative paths

--- compatibility/BaseCompatibility.cpp
+++ compatibility/BaseCompatibility.cpp
@@ -72,12 +72,11 @@ namespace compatibility
                     uri = std::regex_replace(uri, std::regex(R"(samples\/[^\/]+)"), devkit.folder);
                     uri = std::regex_replace(uri, std::regex(R"(\.ts$)"), ".cpp");
 
-                    fmt::println("current path: {}", std::filesystem::current_path().string());
-                    fmt::println("   base path: {}", CCR_SOURCE_DIR);
-                    fmt::println(" binary path: {}", CCR_BINARY_DIR);
-                    fmt::println("    uri path: {}", uri);
+                    std::filesystem::path path{ uri };
+                    if (path.is_relative())
+                        path = std::filesystem::path{ CCR_BINARY_DIR } / path;
 
-                    json[key] = std::filesystem::canonical(uri).string();
+                    json[key] = std::filesystem::canonical(path).string();
 
                     ++jsonIter;
                 }
